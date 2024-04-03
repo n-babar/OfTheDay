1eef68a6a1062d37a3d9156c3118156e5e1e7840
ejbh24 <70079578+ejbh24@users.noreply.github.com>
2024-04-03 00:06:07 -0700

    Add files via upload

---
+++ Feed-Navigator.js
@@ -0,0 +1,21 @@
+// Initializes and configures navigation for the feed section, including Friends and Global feeds.
+
+import { createStackNavigator } from '@react-navigation/stack';
+import FriendsFeedPage from "./FriendsFeed.js";
+import GlobalFeedPage from './GlobalFeed.js';
+
+const Stack = createStackNavigator();
+
+const FeedStack = () => {
+    return (
+        <Stack.Navigator 
+            screenOptions={{
+                headerShown: false
+            }}>
+        <Stack.Screen name="FriendsFeed" component={FriendsFeedPage} />
+        <Stack.Screen name="GlobalFeed" component={GlobalFeedPage} />
+        </Stack.Navigator>
+    );
+};
+
+export default FeedStack;
